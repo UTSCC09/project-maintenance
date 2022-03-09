bdd170752244a14c3fa3f6767f39c058dde10a7e
Shaopeng (Chris) Lin <shaopeng.lin@mail.utoronto.ca>
2022-03-09 11:32:43 -0500

    Changed Apollo server to use express
    Now uses express app as middleware for server. Runs on port 3000 with /graphql as playground. This change is to enable express sessions for user authentication with passport

--- dr.handyman/index.js
+++ dr.handyman/index.js
@@ -1,11 +1,25 @@
-const { ApolloServer, gql } = require('apollo-server');
-
+const { ApolloServer, gql } = require('apollo-server-express');
 const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const uuid = require('uuid').v4;
+
 const {workerDataMutDef, workerDataDefs, WorkerData, workerDataMut} = require('./workerDataSchema');
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const connection = "mongodb+srv://Chris:chris@dr-handyman.7i3hz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 mongoose.connect(connection);
+
+const SESSION_SECRET = 'some secret';
+
+const app = express();
+
+app.use(session({
+  genid: (req) => uuid(),
+  secret: SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+}));
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
@@ -43,9 +57,21 @@ resolvers.Mutation = Object.assign({}, resolvers.Mutation, workerDataMut)
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs, resolvers });
+let server = null;
+async function startServer() {
+    server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        introspection: true,
+        playground: true,
+    });
+    await server.start();
+    server.applyMiddleware({ app });
+}
+startServer();
 
 // The `listen` method launches a web server.
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+app.listen(3000, function () {
+  console.log(`server running on port 3000`);
+  console.log(`gql path is localhost:3000${server.graphqlPath}`);
 });
