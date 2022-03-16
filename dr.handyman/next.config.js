98c98382b9578680bf281fb3d3af3441a9c5d67a
Yuming Liu <yuming.liu@mail.utoronto.ca>
2022-03-15 22:35:02 -0400

    post queries partially complete, render problem still exists

--- dr.handyman/next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  devIndicators: {
-    autoPrerender: false,
-  },
-  publicRuntimeConfig: {
-    // Will be available on both server and client
-    theme: "DEFAULT",
-  },
-};
