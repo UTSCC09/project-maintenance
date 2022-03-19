0b6377856338e1b254c9e479f07886ba6cd686ec
Shaopeng (Chris) Lin <shaopeng.lin@mail.utoronto.ca>
2022-03-18 23:49:19 -0400

    Added socket.io basic interface for webrtc

--- dr.handyman/index.js
+++ dr.handyman/index.js
@@ -85,7 +85,6 @@ require('dotenv').config();
     }
   });
   app.use(sessionMid);
-  // app.use(express.static('files'));
 
   app.get('/pictures/:email', async (req, res) => {
     if (!req.session.passport)
@@ -141,6 +140,28 @@ require('dotenv').config();
 // });
 // Express X Passport X HTTPS setup
 
+const io = require("socket.io")(httpServer, {
+	cors: {
+		origin: "*",
+		methods: [ "GET", "POST" ]
+	}
+})
+
+io.on("connection", (socket) => {
+	socket.emit("me", socket.id)
+
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callEnded")
+	})
+
+	socket.on("callUser", (data) => {
+		io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
+	})
+
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	})
+})
 // Initialize and start the HTTPS server
   async function startServer() {
       // Creating the WebSocket server
