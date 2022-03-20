31956dada2468d4d1021a2c56b51aa7d60fd697e
Shaopeng (Chris) Lin <shaopeng.lin@mail.utoronto.ca>
2022-03-20 15:16:44 -0400

    Nevermind

--- dr.handyman/graphqlSchema.js
+++ dr.handyman/graphqlSchema.js
@@ -12,11 +12,11 @@ const { chatMutDef, chatQueryDef, chatDefs, Conversation, Message, chatMut, chat
 const { fileUploadDef, fileUploadMut, fileUploadMutDef, fileUploadQueryDef, fileUploadScalar } = require('./userProfilePicUpload');
 
 async function addUser (parent, args, context, info) {
-    const { email, password, username, phone } = args;
+    const { email, password, username } = args;
     const userObj = new User({
         email, password, username,
         type: "user",
-        phone,
+        phone: "N/A",
         rating: 0,
         permissions: []
     });
@@ -57,7 +57,7 @@ const typeDefs = gql(`
     # Mutation Types
     type Mutation {
         login(email: String!, password: String!): AuthPayload
-        signup(username: String!, email: String!, password: String!, phone: String!): AuthPayload
+        signup(username: String!, email: String!, password: String!): AuthPayload
         logout: Boolean`+ 
         workerDataMutDef + 
         userMutDef + 
@@ -105,7 +105,7 @@ const resolvers = {
             return { user };
         },
         logout: (parent, args, context) => context.logout(),
-        signup: async (parent, { username, email, password, phone }, context) => {
+        signup: async (parent, { username, email, password}, context) => {
             const existUser = await User.findOne({ email: email});
             if (existUser) {
                 throw new Error('User with email already exists');
@@ -115,7 +115,7 @@ const resolvers = {
                     if (err) reject(Error('salt gen failed'));
                     bcrypt.hash(password, salt, async (err, hash) => {
                         if (err) reject(Error('hash failed'));
-                        const newUser = await addUser(null, {email, username, password: hash, phone}, null, null);
+                        const newUser = await addUser(null, {email, username, password: hash}, null, null);
                         await context.login(newUser);
                         resolve({ user: newUser });
                     });
