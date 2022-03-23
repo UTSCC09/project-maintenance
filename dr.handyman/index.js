a6602247560101a6aa4c2c9ec55132444a45ff8b
ShaopengLin <shaopeng.lin@mail.utoronto.ca>
2022-03-23 22:23:34 +0000

    new protocal

--- dr.handyman/index.js
+++ dr.handyman/index.js
@@ -72,7 +72,7 @@ require('dotenv').config();
     saveUninitialized: false,
     cookie: {
       maxAge: 360000,
-      secure: false,
+      secure: true,
       httpOnly: false,
       sameSite: 'none',
     }
