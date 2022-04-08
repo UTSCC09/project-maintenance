93bd753decda7fc135dfb55a1563607386c2a555
ShaopengLin <shaopeng.lin@mail.utoronto.ca>
2022-04-07 21:23:20 -0400

    Reverting back, going to create new microservice

--- dr.handyman/index.js
+++ dr.handyman/index.js
@@ -188,8 +188,7 @@ require('dotenv').config();
 
 // Socket io mid-point for video initialization
 // Reference Number: 3
-  const socketHttp = http.createServer(app);
-  const io = require("socket.io")(socketHttp, {
+  const io = require("socket.io")(httpServer, {
     cors: {
       credentials: true,
       origin: process.env.PROD == 'false' ? ['https://www.drhandyman.me', 'http://localhost:3001'] : ['https://www.drhandyman.me'],
@@ -271,11 +270,6 @@ require('dotenv').config();
         io.to(socket.id).emit("callEnded", "Error when trying to find user");
       });
     })
-  });
-  socketHttp.listen(5000, function (err) {
-    if (err) console.log(err);
-    else console.log("HTTPS server on https://localhost:%s", "5000");
-    
   });
 // Socket io mid-point for video initialization
 
